7f8b8280b8689d4f64807922ad928d78051311dd
SK <senthilptj@gmail.com>
2022-03-03 08:35:35 +0530

    Mood Analyser - JUnit

---
+++ src/main/java/com/bridgelabz/day21/MoodAnalyser.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.day21;
+
+public class MoodAnalyser {
+    String message;
+    public MoodAnalyser(){
+
+    }
+    public MoodAnalyser(String message) {
+        super();
+        this.message = message;
+    }
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0) {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, "Enter Proper Message. EMPTY Not Allowed");
+            }
+            if (this.message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL, "Enter Proper Message. NULL Not Allowed");
+        }
+    }
+}
